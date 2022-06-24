e93346b3fffe789acb08cd952869acdd71a5b689
dan9401 <dansummer94@gmail.com>
2022-06-24 11:14:57 -0400

    add .py files

---
+++ models/preprocess_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/nmzpAgeSexFL.csv")
+df = df.drop(columns=['Unnamed: 0', 'zip5', 'sex', 'age'])
+df.columns = ['first_name', 'last_name', 'race_cat']
+
+def process_florida_voter_dataset(df, include_race=['aian', 'api', 'nh_black', 'hispanic', 'nh_white']):
+    
+    cat_race_map = {1: "aian", 2: "api", 3: "nh_black", 4: "hispanic", 5: "nh_white", 6: "other", 7: "multi_racial", 9: "unknown"}
+    
+    df['race'] = df['race_cat'].map(cat_race_map)
+    df = df[df['race'].isin(include_race)]
+
+    # 0 = Asian, 1 = Black, 2=Hispanic, 3=White
+    label_race_map = dict((enumerate(df["race"].value_counts().index)))
+    race_label_map = {v: k for k, v in label_race_map.items()}
+    
+    df["label"] = df.loc[:, "race"].map(race_label_map)
+    # df['label'].value_counts()
+
+    df = df[["first_name", "last_name", "race", "label"]]
+
+    df["first_last"] = df["first_name"].str.lower() + "_" + df["last_name"].str.lower()
+
+    df = df[df["first_last"].notnull()]
+
+    df_train, df_holdout = train_test_split(df, train_size=0.9, test_size=0.1)
+    
+    # return df_train, df_holdout
+    df_train.to_parquet(f"data/florida_{len(include_race)}label_train.parquet")
+    df_holdout.to_pickle(f"data/florida_{len(include_race)}label_test.parquet")
+    
+if __name__ == "__main__":
+    import sys
+    
+    data_file = sys.argv[1]
+    
+    df = pd.read_csv(data_file)
+    df = df.drop(columns=['Unnamed: 0', 'zip5', 'sex', 'age'])
+    df.columns = ['first_name', 'last_name', 'race_cat']
+    
+    process_florida_voter_dataset(df)
+    
+    
---
+++ models/raceBERT_train.py
@@ -0,0 +1,230 @@
+from transformers import (
+    AutoTokenizer,
+    PreTrainedTokenizerFast,
+    BertForSequenceClassification,
+    RobertaForSequenceClassification,
+    RobertaConfig,
+    Trainer,
+    TrainingArguments,
+    EvalPrediction,
+)
+from models.t5_extra_models import T5ForSequenceClassification
+from tokenizers import Tokenizer
+import transformers
+from transformers.utils import logging
+import wandb
+from sklearn.metrics import precision_recall_fscore_support
+import datasets
+import logging
+import pandas as pd
+import torch
+import pandas as pd
+
+
+# logging
+transformers.logging.set_verbosity_info()
+
+# args, modify the batch size here to accomodate the vram size
+N_EPOCHS = 4
+BATCH_SIZE_PER_GPU = 256
+LEARNING_RATE = 2e-5
+WEIGHT_DECAY = 2e-5
+
+
+def get_label_race_map(dataset_name: str) -> dict:
+    """
+    get label:race map
+    e.g. {0: white, 1: api}
+    """
+    df = pd.read_parquet(f"data/{dataset_name}_train.parquet")
+    label_race_map = dict(enumerate(df["race"].value_counts().index))
+    return label_race_map
+
+
+def compute_metrics(pred: EvalPrediction) -> dict:
+
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    metrics = dict()
+
+    # avg metrics
+    avg_precision, avg_recall, avg_f1, avg_support = precision_recall_fscore_support(
+        labels, preds, average="weighted"
+    )
+    metrics["avg_precision"] = avg_precision
+    metrics["avg_recall"] = avg_recall
+    metrics["avg_f1"] = avg_f1
+
+    # race level metrics
+    precisions, recalls, f1s, supports = precision_recall_fscore_support(
+        labels, preds, average=None, labels=list(label_race_map.keys())
+    )
+
+    for label, value in enumerate(precisions):
+        metrics[f"{label_race_map[label]}_precision"] = value  # e.g. api_precision
+
+    for label, value in enumerate(recalls):
+        metrics[f"{label_race_map[label]}_recall"] = value
+
+    for label, value in enumerate(f1s):
+        metrics[f"{label_race_map[label]}_f1"] = value
+
+    return metrics
+
+# added raceBERT model, pay attention to name_col to be the right one
+def get_trainer(model_name, dataset_name):
+
+    # load dataset
+    dataset = datasets.load_dataset(
+        "parquet",
+        data_files={"train": f"data/{dataset_name}_train.parquet"},
+        split="train",
+    )
+    # num labels
+    num_labels = len(set(dataset["label"]))
+
+    # load model
+    if model_name == "bert":
+        logging.info("instantiating model")
+
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
+        model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
+        name_col = "first_last"
+    elif model_name == "charbert":
+        logging.info("instantiating model")
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            "trained_models/charbert-bert-wiki", use_fast=True
+        )
+        model = BertForSequenceClassification.from_pretrained(
+            "trained_models/charbert-bert-wiki",
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
+        name_col = "first_last"
+    elif model_name == "raceBERT":
+
+
+        tokenizer = AutoTokenizer.from_pretrained("pparasurama/raceBERT", use_fast=True)
+
+        model = RobertaForSequenceClassification.from_pretrained("pparasurama/raceBERT", 
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,)
+        name_col = "first_last"
+    elif model_name == "namebert-roberta":
+        
+        tokenizer_obj = Tokenizer.from_file("trained_models/char-tokenizer")
+
+        tokenizer = PreTrainedTokenizerFast(
+            tokenizer_object=tokenizer_obj, return_special_tokens_mask=True
+        )
+        tokenizer.pad_token = "[PAD]"
+        tokenizer.mask_token = "[MASK]"
+
+        model = RobertaForSequenceClassification.from_pretrained(
+            "trained_models/colorful-yogurt-9",
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
+        name_col = "first LAST"
+    elif model_name == "byt5":
+        tokenizer = AutoTokenizer.from_pretrained("google/byt5-small", use_fast=True)
+        model = T5ForSequenceClassification.from_pretrained(
+            "google/byt5-small",
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
+        name_col = "first LAST"
+    else:
+        raise NotImplementedError("not implemented")
+
+    print("\ntokenize\n")
+    # tokenize
+    dataset =  dataset.shuffle(seed=42).select(range(200000))
+    dataset = dataset.map(
+        lambda batch: tokenizer(batch[name_col], truncation=True, padding=True),
+        batched=True,
+    )
+
+    # rename
+    dataset = dataset.rename_column("label", "labels")
+
+    # set format
+    dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+
+    # train/test split
+    dataset = dataset.train_test_split(test_size=0.1)
+    n_train = len(dataset["train"])
+    
+    gpu_count = torch.cuda.device_count()
+    print("\ngpucount: "+str(gpu_count)+"\n")
+    total_opt_steps = int((n_train * N_EPOCHS) / (BATCH_SIZE_PER_GPU * gpu_count))
+
+    MODEL_OUTPUT_DIR = f"trained_models/{run_name}"
+    WARMUP = int(0.1 * n_train / (BATCH_SIZE_PER_GPU * gpu_count))
+    SAVE_STEPS = int(total_opt_steps / 10)  # save every 10th of the way
+    LOGGING_STEPS = int(total_opt_steps / 20)  # log every 20th of the way
+    EVAL_STEPS = int(total_opt_steps / 20)  # eval every 20th of the way
+
+    training_args = TrainingArguments(
+        output_dir=MODEL_OUTPUT_DIR,  # output directory
+        num_train_epochs=N_EPOCHS,  # total # of training epochs
+        per_device_train_batch_size=BATCH_SIZE_PER_GPU,  # batch size per device during training
+        per_device_eval_batch_size=BATCH_SIZE_PER_GPU,  # batch size for evaluation
+        learning_rate=LEARNING_RATE,
+        warmup_steps=WARMUP,
+        weight_decay=WEIGHT_DECAY,  # strength of weight decay
+        logging_dir="./logs",  # directory for storing logs
+        logging_steps=LOGGING_STEPS,
+        evaluation_strategy="steps",
+        eval_steps=EVAL_STEPS,
+        save_steps=SAVE_STEPS,
+        save_total_limit=2,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,  # training arguments, defined above
+        train_dataset=dataset["train"],  # training dataset
+        eval_dataset=dataset["test"],  # evaluation dataset
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
+    return trainer
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+    
+    # wandb = weights and bias, a training monitoring platform, disabled here as it may creates more confusion in the agency environment
+    os.environ["WANDB_DISABLED"] = "true"
+    # wandb.init(project="raceBERT")
+    # run_name = wandb.run.name
+    
+    # probably name this something meaningful
+    run_name = 'test'
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(device)
+
+    model_name = sys.argv[1]
+    dataset_name = sys.argv[2]
+
+    label_race_map = get_label_race_map(dataset_name)
+    trainer = get_trainer(model_name=model_name, dataset_name=dataset_name)
+
+    # wandb config, disabled here
+    # wandb.config.batch_size = BATCH_SIZE_PER_GPU
+    # wandb.config.learning_rate = LEARNING_RATE
+    # wandb.config.weight_decay = WEIGHT_DECAY
+
+    trainer.train()
+    trainer.save_model(f"trained_models/{run_name}")
